3fa6494eee8fa9281e3a34653bd6e3e0f32d07ac
rainbows22 <aishwarya.gunasekar@yahoo.com>
2014-07-27 14:43:58 -0500

    Update BS_index.py

--- BS_index.py
+++ BS_index.py
@@ -1,18 +1,24 @@
 def Binary_Search(my_Array,num_to_search):
   first = 0;
   last = len(my_Array) - 1
-  while(first <= last): 
+  not_found = -1
+  r1 = False
+  r2 = -1;
+
+  while(first <= last):
     middle = (first + last)//2
     if my_Array[middle] ==num_to_search:
-      print middle;
-      print ("is the index");
-      return True
+     r1 = True
+     r2 = middle
+     return (r1,r2)
     elif(my_Array[middle]< num_to_search):
       first = middle + 1
     elif(my_Array[middle]> num_to_search):
       last = middle-1
-    else:
-      return False
+  else:
+
+    #print (r2);
+    return (r1,r2)
 if __name__ == "__main__":
   my_Array_S = []
   my_Array = []
@@ -25,8 +31,7 @@ if __name__ == "__main__":
   my_Array = list(map(int, my_Array_S))
 # ====
   num_to_search = int(input("Number to be searched in the Sorted Array?"))
-  if Binary_Search(my_Array,num_to_search):
-    print(" where the number is Found!!")
-  else:
-    print("Nah!")
-    
+  #if Binary_Search(my_Array,num_to_search):
+  r1, r2 =  Binary_Search(my_Array,num_to_search)
+  print r1;
+  print r2;
